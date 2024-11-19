db03b2746668d0a8385bb4d452b4cf3df0ab1c8f
Arista indrajaya <arista.indrajaya@gmail.com>
2024-11-19 17:09:02 +0700

    build: Implement posthog for user analyze

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
 import '../styles/globals.css'
 // Import thirdweb provider and Rinkeby ChainId
 import { ThirdwebProvider } from '@thirdweb-dev/react';
+import posthog from 'posthog-js';
+import { PostHog } from 'posthog-js/react';
 
-function MyApp({ Component, pageProps }) {
+if (typeof window !== 'undefined') { // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <div>
-      <ThirdwebProvider activeChain="sepolia" clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}>
-        <Component {...pageProps} />
-      </ThirdwebProvider>
+      <PostHogProvider client={posthog}>
+        <ThirdwebProvider activeChain="sepolia" clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}>
+          <Component {...pageProps} />
+        </ThirdwebProvider>
+      </PostHogProvider>
     </div>
   );
 }
