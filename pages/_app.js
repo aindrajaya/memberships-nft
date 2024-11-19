e7d9975a04acc318896253c86a47b67bfdcba3c5
Arista indrajaya <arista.indrajaya@gmail.com>
2024-11-19 17:10:19 +0700

    buid: update posthog ID

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import posthog from 'posthog-js';
 import { PostHog } from 'posthog-js/react';
 
 if (typeof window !== 'undefined') { // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_ID, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     loaded: (posthog) => {
