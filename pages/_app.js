ba05ea1e9c7126b14299f698dad4cd595cd186fd
Arista indrajaya <arista.indrajaya@gmail.com>
2024-11-19 17:14:11 +0700

    update: fix misconfig for posthog

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 // Import thirdweb provider and Rinkeby ChainId
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import posthog from 'posthog-js';
-import { PostHog } from 'posthog-js/react';
+import { PostHogProvider } from 'posthog-js/react';
 
 if (typeof window !== 'undefined') { // checks that we are client-side
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_ID, {
